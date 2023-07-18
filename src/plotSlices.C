eb0a1e87cc0125249464918774580f42e79e30e2
gajjugrg <gajendra.315.gurung@gmail.com>
2023-07-18 18:30:11 -0500

    added flexibility

--- src/plotSlices.C
+++ src/plotSlices.C
@@ -5,59 +5,55 @@
 #include <TFile.h>
 #include <TTree.h>
 
-
 void plotSlices(const char* inputfilename, const char* outputFilename){
-	TFile* file = new TFile(inputfilename, "read");
-	TTree* ana = (TTree*)file->Get("brem");
+    TFile* file = new TFile(inputfilename, "read");
+    TTree* ana = (TTree*)file->Get("brem");
+    Double_t nEntries = ana->GetEntries();
 
-	Double_t nEntries = ana->GetEntries();
+    cout << "The root file has : "<< nEntries << " Entries" << endl;
+    Int_t           TrackID;
+    Double_t        fKineticEnergy;
+    Double_t        fTrackLength_X0;
+    Char_t          fProcess[16];
+    // List of branches
+    ana->SetBranchAddress("TrackID" , &TrackID);
+    ana->SetBranchAddress("fKineticEnergy", &fKineticEnergy);
+    ana->SetBranchAddress("fTrackLength_X0", &fTrackLength_X0);
+    ana->SetBranchAddress("fProcess", &fProcess);
+    Double_t Energy[3] = {.1, .3, .5};//1 GeV case
+    //  Double_t Energy[3] = {1, 3, 5};  //10 GeV case
+    //	Double_t Energy[3] = {10, 30, 45}; //100 GeV case
+    cout << Energy [0] << " ------ " << Energy[1] <<" ------ " << Energy[2] << endl; 
+    // Initialize the 1-D histogram for each slice of energy
+    TH1D* slice0 = new TH1D("slice0", "; Depth in units of Radiation Length; Probability / Bin ", 100, 0, 10);
+    slice0 -> SetLineColor(8);
+    TH1D* slice1 = new TH1D("slice1", "; Depth in units of Radiation Length; Probability / Bin ", 100, 0, 10);
+    slice1 -> SetLineColor(6);
+    TH1D* slice2 = new TH1D("slice2", "; Depth in units of Radiation Length; Probability / Bin", 100, 0, 10);
+    slice2 -> SetLineColor(4);
 
-	cout << "The root file has : "<< nEntries << " Entries" << endl;
-	Int_t           TrackID;
-	Double_t        fKineticEnergy;
-	Double_t        fTrackLength_X0;
-	Char_t          fProcess[16];
-	// List of branches
-	ana->SetBranchAddress("TrackID" , &TrackID);
-	ana->SetBranchAddress("fKineticEnergy", &fKineticEnergy);
-	ana->SetBranchAddress("fTrackLength_X0", &fTrackLength_X0);
-	ana->SetBranchAddress("fProcess", &fProcess);
-
-        //Double_t Energy[3] = {1, 3, 5};  //10 GeV case
-	Double_t Energy[3] = {10, 30, 45}; //100 GeV case
-	// Initialize the 1-D histogram for each slice of energy
-	TH1D* slice0 = new TH1D("slice0", "; Depth in units of Radiation Length; Probability / Bin ", 100, 0, 10);
-	slice0 -> SetLineColor(8);
-	TH1D* slice1 = new TH1D("slice1", "; Depth in units of Radiation Length; Probability / Bin ", 100, 0, 10);
-	slice1 -> SetLineColor(6);
-	TH1D* slice2 = new TH1D("slice2", "; Depth in units of Radiation Length; Probability / Bin", 100, 0, 10);
-	slice2 -> SetLineColor(4);
-	
-	// Filling the histograms
-	for(Int_t i = 0; i < ana->GetEntries(); i ++){
-		ana->GetEntry(i);
-		if( TrackID != 1) continue;
-		if(fKineticEnergy >= (Energy[0]*1000-10) && fKineticEnergy < (Energy[0]*1000+10)){slice0->Fill(fTrackLength_X0);}
-		if(fKineticEnergy >= (Energy[1]*1000-10) && fKineticEnergy < (Energy[1]*1000+10)){slice1->Fill(fTrackLength_X0);}
-		if(fKineticEnergy >= (Energy[2]*1000-10) && fKineticEnergy < (Energy[2]*1000+10)){slice2->Fill(fTrackLength_X0);}
-	}
-	/*auto c = new TCanvas("c","c");
-	slice0->DrawNormalized("e1p");
-	slice1->DrawNormalized("e1p same");
-	slice2->DrawNormalized("e1p same");
-	c->SetLogy(1);
-	gStyle->SetOptStat(0); //this removes the Statbox
-	auto legend = new TLegend();
-	legend->SetHeader("Legend","C");
-	legend->AddEntry(slice0, Form("%.f GeV", Energy[0]) ,"l");
-	legend->AddEntry(slice1, Form("%.f GeV", Energy[1]) ,"l");
-	legend->AddEntry(slice2, Form("%.f GeV", Energy[2]) ,"l");
-	legend->Draw();
-	*/
-	TFile f(outputFilename,"recreate");
-	slice0->Write();
-	slice1->Write();
-	slice2->Write();
+    // Filling the histograms
+    for(Int_t i = 0; i < ana->GetEntries(); i ++){
+        ana->GetEntry(i);
+        if( TrackID != 1) continue;
+        if(fKineticEnergy >= (Energy[0]*1000-100) && fKineticEnergy < (Energy[0]*1000+100)){slice0->Fill(fTrackLength_X0);}
+        if(fKineticEnergy >= (Energy[1]*1000-100) && fKineticEnergy < (Energy[1]*1000+100)){slice1->Fill(fTrackLength_X0);}
+        if(fKineticEnergy >= (Energy[2]*1000-100) && fKineticEnergy < (Energy[2]*1000+100)){slice2->Fill(fTrackLength_X0);}
+    }
+    auto c = new TCanvas("c","c");
+    slice0->DrawNormalized("e1p");
+    slice1->DrawNormalized("e1p same");
+    slice2->DrawNormalized("e1p same");
+    c->SetLogy(1);
+    gStyle->SetOptStat(0); //this removes the Statbox
+    auto legend = new TLegend();
+    legend->SetHeader("Legend","C");
+    legend->AddEntry(slice0, Form("%.f GeV", Energy[0]) ,"l");
+    legend->AddEntry(slice1, Form("%.f GeV", Energy[1]) ,"l");
+    legend->AddEntry(slice2, Form("%.f GeV", Energy[2]) ,"l");
+    legend->Draw();
+    TFile f(outputFilename,"recreate");
+    slice0->Write();
+    slice1->Write();
+    slice2->Write();
 }
-
-
