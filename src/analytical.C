683979cfa71cdaa991016291f137d4353564c2ff
Gajendra Gurung <gajendra.315.gurung@gmail.com>
2023-07-18 18:28:12 -0500

    added flexibilty

--- src/analytical.C
+++ src/analytical.C
@@ -1,47 +1,52 @@
 // Author: Gajendra Gurung
-// University of Texas at Arlington
-// This is the function to plot the analytical data: equation 22 from https://doi.org/10.1103/PhysRev.149.1248
+// UT at Arlington
+// This is the function to plot the analytical data: 
+// equation 22 from https://doi.org/10.1103/PhysRev.149.1248
+
 Double_t Intensity(Double_t E_0, Double_t E, Double_t t){
-	return (1/E_0)*(1/tgamma(4*t/3))*pow(log(E_0/E),4*t/3-1);
+    return (1/E_0)*(1/tgamma(4*t/3))*pow(log(E_0/E),4*t/3-1);
 }
 void analytical(){
-	Int_t N = 20000;
-	Double_t E_0 = 100e9 ; //Initial Energy of the electron
-	//Double_t E_i[3] = {90e6, 290e6, 690e6}; //Energy slice starting E_slice - 10 MeV
-	
-	Double_t E_i[3] = {9990e6, 29990e6, 44990e6}; //Energy slice starting E_slice - 10 MeV
-	
-	Double_t delta_t = 0.001; //Step in track length
-	Double_t delta_E = 0.001; //Step in Energy (for +- 10 MeV)
-	Double_t t = 0.0; //Initial Track length
-	Double_t Prob; //Place holder for probability
+    Int_t N = 20000;
+    Double_t E_0 = 10 ; //Initial Energy of the initial electron shot 
+//    Double_t E_i[3] = {, , };     //Energy slice 1 GeV
+    Double_t E_i[3] = {1, 3, 5};  //Energy slice 10 GeV
+//    Double_t E_i[3] = {10, 30, 45}; //Energy slice 100 GeV
 
-	TH1F* slice[3];
-	for(Int_t i = 0 ; i < 3 ; i++){
-		TString name = Form("slice%d", i);
-		slice[i] = new TH1F(name , "; Depth in units of Radiation Length; Probability / Bin", 100, 0, 10);
-		slice[i] -> SetLineColor(i+1);
-	}
+    Double_t delta_t = 0.001; //Step in track length
+    Double_t delta_E = 0.00001; //Step in Energy (for +- 100 MeV)
+    Double_t t = 0.0;         //Initial Track length
+    Double_t Prob;            //Place holder for probability
 
-	for (Int_t g = 0; g < 3; g++) {
-		for (Int_t i = 0; i < N; i++) {
-			Double_t Ei = E_i[g];
-			Prob = 0.;
-			t = i*delta_t;
-			Ei = Ei + i*delta_E;
-			Prob = Prob + Intensity(E_0, Ei, t);
-			slice[g] -> Fill(t,Prob);
-		}
-	}
+    // Initializing the histogram slices
+    TH1F* slice[3];
+    for(Int_t i = 0 ; i < 3 ; i++){
+        TString name = Form("slice%d", i);
+        slice[i] = new TH1F(name , "; Depth in units of Radiation Length; Probability / Bin", 100, 0, 10);
+        slice[i] -> SetLineColor(i+1);
+    }
 
-	TCanvas* c = new TCanvas("c", "Track Length distribution slices at different energies");
-	slice[0] -> Draw();
-	slice[1] -> Draw("same");
-	slice[2] -> Draw("same");
-	
-	TFile f("../outputfiles/slicesAna_100GeV.root","recreate");
-	slice[0]->Write();
-	slice[1]->Write();
-	slice[2]->Write();
+    // Here g is for each energy value
+    for (Int_t g = 0; g < 3; g++) {
+            Double_t Ei = E_i[g] - 0.10;
+            t = 0.0;
+            Prob = 0.0;
+            for (Int_t i = 0; i < N; i++) {
+                Prob = Intensity(E_0, Ei, t);
+                slice[g] -> Fill(t,Prob);
+//                cout << "g; " << g << " t: " << t << " Ei: " << Ei << " Prob: " << Prob << endl;
+                t = t + delta_t;
+                Ei = Ei + delta_E;
+            }
+    }
 
+    TCanvas* c = new TCanvas("c", "Track Length distribution slices at different energies");
+    slice[0] -> DrawNormalized("hist");
+    slice[1] -> DrawNormalized("hist same");
+    slice[2] -> DrawNormalized("hist same");
+
+    TFile f(Form("../outputfiles/slices/slicesAna_%.fGeV.root", E_0),"recreate");
+    slice[0] -> Write();
+    slice[1] -> Write();
+    slice[2] -> Write();
 }
--- src/pseudoscalar.C
+++ src/pseudoscalar.C
@@ -1,94 +1,95 @@
 void pseudoscalar(){
-  Double_t Energy[3] = {10, 30, 45}; //In GeV
-  
-  TH1F* Ana_slice[3];
-  TFile* g = new TFile("../outputfiles/slicesAna_100GeV.root");
-  Ana_slice[0] = (TH1F*)g -> Get("slice0"); Ana_slice[0] -> SetLineColor(7);
-  Ana_slice[1] = (TH1F*)g -> Get("slice1"); Ana_slice[1] -> SetLineColor(7);
-  Ana_slice[2] = (TH1F*)g -> Get("slice2"); Ana_slice[2] -> SetLineColor(7);
-  
-  TH1F* MC_slice[3];
-  TFile* f = new TFile("../outputfiles/slicesG4_100GeV.root");
-  MC_slice[0] = (TH1F*)f -> Get("slice0"); MC_slice[0] -> SetLineColor(9); MC_slice[0] -> Sumw2();
-  MC_slice[1] = (TH1F*)f -> Get("slice1"); MC_slice[1] -> SetLineColor(9); MC_slice[1] -> Sumw2();
-  MC_slice[2] = (TH1F*)f -> Get("slice2"); MC_slice[2] -> SetLineColor(9); MC_slice[2] -> Sumw2();
-  
-  TH1F* DMG4_e0_slice[3];
-  TFile* i = new TFile("../outputfiles/slicesDMG4_100GeV_bias_e9.root");
-  DMG4_e0_slice[0] = (TH1F*)i -> Get("slice0"); DMG4_e0_slice[0] -> SetLineColor(2);
-  DMG4_e0_slice[1] = (TH1F*)i -> Get("slice1"); DMG4_e0_slice[1] -> SetLineColor(2);
-  DMG4_e0_slice[2] = (TH1F*)i -> Get("slice2"); DMG4_e0_slice[2] -> SetLineColor(2);
-  
-  TH1F* DMG4_e3_slice[3];
-  TFile* h = new TFile("../outputfiles/slicesDMG4_100GeV_bias_e-5.root");
-  DMG4_e3_slice[0] = (TH1F*)h -> Get("slice0"); DMG4_e3_slice[0] -> SetLineColor(1);
-  DMG4_e3_slice[1] = (TH1F*)h -> Get("slice1"); DMG4_e3_slice[1] -> SetLineColor(1);
-  DMG4_e3_slice[2] = (TH1F*)h -> Get("slice2"); DMG4_e3_slice[2] -> SetLineColor(1);
-  
-  gStyle->SetOptStat(0);
-  gStyle->SetLegendFont(80);
-  
-  TCanvas* c = new TCanvas("c", Form("Ratio of Probability distribution at %.f MeV",Energy[0]));
-  Ana_slice[0]->Scale(.1/Ana_slice[0]->Integral("width"));
-  MC_slice[0]->Scale(.1/MC_slice[0]->Integral(), "width");
-  DMG4_e0_slice[0]->Scale(.1/DMG4_e0_slice[0]->Integral(), "width");
-  DMG4_e3_slice[0]->Scale(.1/DMG4_e3_slice[0]->Integral(), "width");
-  
-  
-  Ana_slice[0]->Draw("hist");
-  MC_slice[0]->Draw("same");
-  DMG4_e0_slice[0]->Draw("hist same");
-  DMG4_e3_slice[0]->Draw("hist same");
-  
-  
-  auto leg = new TLegend();
-  leg->SetHeader("Legend","C");
-  leg->AddEntry(Ana_slice[0],"Ana","le");
-  leg->AddEntry(MC_slice[0],"G4","le");
-  leg->AddEntry(DMG4_e0_slice[0],"DMG4 bais e9","le");
-  leg->AddEntry(DMG4_e3_slice[0],"DMG4 bais e-15","le");
-  
-  leg->Draw();
-  
-  TCanvas* c1 = new TCanvas("c1", Form("Ratio of Probability distribution at %.f MeV",Energy[1]));
-  Ana_slice[1]->Scale(.1/Ana_slice[1]->Integral("width"));
-  MC_slice[1]->Scale(.1/MC_slice[1]->Integral(), "width");
-  DMG4_e0_slice[1]->Scale(.1/DMG4_e0_slice[1]->Integral(), "width");
-  DMG4_e3_slice[1]->Scale(.1/DMG4_e3_slice[1]->Integral(), "width");
-  
-  
-  Ana_slice[1]->Draw("hist");
-  MC_slice[1]->Draw("same");
-  DMG4_e0_slice[1]->Draw("hist same");
-  DMG4_e3_slice[1]->Draw("hist same");
-  
-  
-  auto leg1 = new TLegend();
-  leg1->SetHeader("Legend","C");
-  leg1->AddEntry(Ana_slice[1]," Ana","le");
-  leg1->AddEntry(MC_slice[1],"G4","le");
-  leg1->AddEntry(DMG4_e0_slice[1],"DMG4 bais e9","le");
-  leg1->AddEntry(DMG4_e3_slice[1],"DMG4 bais e-15","le");
-  leg1->Draw();
-  
-  TCanvas* c2 = new TCanvas("c2", Form("Ratio of Probability distribution at %.f MeV",Energy[2]));
-  Ana_slice[2]->Scale(.1/Ana_slice[2]->Integral("width"));
-  MC_slice[2]->Scale(.1/MC_slice[2]->Integral(), "width");
-  DMG4_e0_slice[2]->Scale(.1/DMG4_e0_slice[2]->Integral(), "width");
-  DMG4_e3_slice[2]->Scale(.1/DMG4_e3_slice[2]->Integral(), "width");
-  
-  
-  Ana_slice[2]->Draw("hist");
-  MC_slice[2]->Draw("same");
-  DMG4_e0_slice[2]->Draw("hist same");
-  DMG4_e3_slice[2]->Draw("hist same");
-  
-  auto leg2 = new TLegend();
-  leg2->SetHeader("Legend","C");
-  leg2->AddEntry(Ana_slice[2],"Ana","le");
-  leg2->AddEntry(MC_slice[2],"G4","le");
-  leg2->AddEntry(DMG4_e0_slice[2],"DMG4 bais e9","le");
-  leg2->AddEntry(DMG4_e3_slice[2],"DMG4 bais e-15","le");
-  leg2->Draw();
-  
+    
+    Double_t E0 = 10; //Initial electron energy in GeV
+    //Double_t Energy[3] = {.1, .3, .5}; //For 1 GeV
+    Double_t Energy[3] = {1, 3, 5}; //For 10 GeV
+    //Double_t Energy[3] = {10, 30, 45}; //for 100 GeV
+
+    TH1F* Ana_slice[3];
+    TFile* g = new TFile(Form("/Users/Gajju/bremAnalysis/outputfiles/slices/slicesAna_%.fGeV.root", E0));
+    Ana_slice[0] = (TH1F*)g -> Get("slice0"); Ana_slice[0] -> SetLineColor(7);
+    Ana_slice[1] = (TH1F*)g -> Get("slice1"); Ana_slice[1] -> SetLineColor(7);
+    Ana_slice[2] = (TH1F*)g -> Get("slice2"); Ana_slice[2] -> SetLineColor(7);
+
+    TH1F* MC_slice[3];
+    TFile* f = new TFile(Form("/Users/Gajju/bremAnalysis/outputfiles/slices/slices_g4_%.fGeV.root", E0));
+    MC_slice[0] = (TH1F*)f -> Get("slice0"); MC_slice[0] -> SetLineColor(9); MC_slice[0] -> Sumw2();
+    MC_slice[1] = (TH1F*)f -> Get("slice1"); MC_slice[1] -> SetLineColor(9); MC_slice[1] -> Sumw2();
+    MC_slice[2] = (TH1F*)f -> Get("slice2"); MC_slice[2] -> SetLineColor(9); MC_slice[2] -> Sumw2();
+
+    TH1F* DMG4_e0_slice[3];
+    TFile* i = new TFile(Form("/Users/Gajju/bremAnalysis/outputfiles/slices/slices_%.fGeV_bias_e7.root", E0));
+    DMG4_e0_slice[0] = (TH1F*)i -> Get("slice0"); DMG4_e0_slice[0] -> SetLineColor(2);
+    DMG4_e0_slice[1] = (TH1F*)i -> Get("slice1"); DMG4_e0_slice[1] -> SetLineColor(2);
+    DMG4_e0_slice[2] = (TH1F*)i -> Get("slice2"); DMG4_e0_slice[2] -> SetLineColor(2);
+
+    TH1F* DMG4_e3_slice[3];
+    TFile* h = new TFile(Form("/Users/Gajju/bremAnalysis/outputfiles/slices/slices_%.fGeV_bias_e-15.root", E0));
+    DMG4_e3_slice[0] = (TH1F*)h -> Get("slice0"); DMG4_e3_slice[0] -> SetLineColor(1);
+    DMG4_e3_slice[1] = (TH1F*)h -> Get("slice1"); DMG4_e3_slice[1] -> SetLineColor(1);
+    DMG4_e3_slice[2] = (TH1F*)h -> Get("slice2"); DMG4_e3_slice[2] -> SetLineColor(1);
+
+    gStyle->SetOptStat(0);
+    gStyle->SetLegendFont(80);
+
+    TCanvas* c = new TCanvas("c", Form("Ratio of Probability distribution at %.f GeV",Energy[0]));
+    Ana_slice[0]->Scale(.1/Ana_slice[0]->Integral("width"));
+    MC_slice[0]->Scale(.1/MC_slice[0]->Integral(), "width");
+    DMG4_e0_slice[0]->Scale(.1/DMG4_e0_slice[0]->Integral(), "width");
+    DMG4_e3_slice[0]->Scale(.1/DMG4_e3_slice[0]->Integral(), "width");
+
+
+    Ana_slice[0]->Draw("hist");
+    MC_slice[0]->Draw("e1p same");
+    DMG4_e0_slice[0]->Draw("e1p same");
+    DMG4_e3_slice[0]->Draw("e1p same");
+
+
+    auto leg = new TLegend();
+    leg->SetHeader("Legend","C");
+    leg->AddEntry(Ana_slice[0],"Ana","le");
+    leg->AddEntry(MC_slice[0],"G4","le");
+    leg->AddEntry(DMG4_e0_slice[0],"DMG4 bias 1e7","le");
+    leg->AddEntry(DMG4_e3_slice[0],"DMG4 bias 1e-15","le");
+    leg->Draw();
+
+    TCanvas* c1 = new TCanvas("c1", Form("Ratio of Probability distribution at %.f GeV",Energy[1]));
+    Ana_slice[1]->Scale(.1/Ana_slice[1]->Integral("width"));
+    MC_slice[1]->Scale(.1/MC_slice[1]->Integral(), "width");
+    DMG4_e0_slice[1]->Scale(.1/DMG4_e0_slice[1]->Integral(), "width");
+    DMG4_e3_slice[1]->Scale(.1/DMG4_e3_slice[1]->Integral(), "width");
+
+    Ana_slice[1]->Draw("hist");
+    MC_slice[1]->Draw("e1p same");
+    DMG4_e0_slice[1]->Draw("e1p same");
+    DMG4_e3_slice[1]->Draw("e1p same");
+
+    auto leg1 = new TLegend();
+    leg1->SetHeader("Legend","C");
+    leg1->AddEntry(Ana_slice[1],"Ana","le");
+    leg1->AddEntry(MC_slice[1],"G4","le");
+    leg1->AddEntry(DMG4_e0_slice[1],"DMG4 bias 1e7","le");
+    leg1->AddEntry(DMG4_e3_slice[1],"DMG4 bias 1e-15","le");
+    leg1->Draw();
+
+    TCanvas* c2 = new TCanvas("c2", Form("Ratio of Probability distribution at %.f GeV",Energy[2]));
+    Ana_slice[2]->Scale(.1/Ana_slice[2]->Integral("width"));
+    MC_slice[2]->Scale(.1/MC_slice[2]->Integral(), "width");
+    DMG4_e0_slice[2]->Scale(.1/DMG4_e0_slice[2]->Integral(), "width");
+    DMG4_e3_slice[2]->Scale(.1/DMG4_e3_slice[2]->Integral(), "width");
+
+
+    Ana_slice[2]->Draw("hist");
+    MC_slice[2]->Draw("same");
+    DMG4_e0_slice[2]->Draw("e1p same");
+    DMG4_e3_slice[2]->Draw("e1p same");
+
+    auto leg2 = new TLegend();
+    leg2->SetHeader("Legend","C");
+    leg2->AddEntry(Ana_slice[2],"Ana","le");
+    leg2->AddEntry(MC_slice[2],"G4","le");
+    leg2->AddEntry(DMG4_e0_slice[2],"DMG4 bias 1e7","le");
+    leg2->AddEntry(DMG4_e3_slice[2],"DMG4 bias 1e-15","le");
+    leg2->Draw();
+
 }
